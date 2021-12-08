22304175837d39a66e964ec968bbd36478cdf218
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2021-12-08 06:21:26 +0530

    Add echo command script

--- 02-print.sh
+++ 02-print.sh
@@ -2,7 +2,12 @@
 
 # Printing in shell can be done by using echo command
 
-# Syntax : echo INPUT MESSSAGE
+# Syntax : echo INPUT MESSAGE
 
 echo Hello
 echo Hello World
+
+## Example from project
+echo Installing Nginx
+echo Starting Nginx Service
+
