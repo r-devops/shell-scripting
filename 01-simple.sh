6be8764797aac23eb00c7c0274e401d1e784bba7
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2021-12-08 06:19:32 +0530

    Add echo command script

--- 01-simple.sh
+++ 01-simple.sh
@@ -4,3 +4,8 @@
 
 ls
 #cat
+
+## TO execute the script we can execute with
+# bash script or sh script
+
+# In Redhat family , BASH and SH are same. IN other OS like Ubuntu those are different.
---
+++ 02-print.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Printing in shell can be done by using echo command
+
+# Syntax : echo INPUT MESSSAGE
+
+echo Hello
+echo Hello World
