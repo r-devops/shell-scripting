76812dcf40b57d42714dc4deca22cfad5d520621
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2021-12-08 06:45:26 +0530

    Create vars and access them

--- 03-variables.sh
+++ 03-variables.sh
@@ -9,3 +9,10 @@ c=true    # Boolean
 
 # Shell does not have any data type by default
 
+# Access the variables, Variable will be access in two ways
+# 1. $VARNAME , $ preceding to the variable name , Ex: $a to access a variable
+# 2. ${VARNAME}, $ preceding along with variable bounded in flower braces, Ex: ${a}
+
+## Best practice is using along with flower braces.
+echo a = $a
+echo a in currency = $$a
