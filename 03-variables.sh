84f2edf068fde213ef988ae74d878d860da40016
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2021-12-08 06:41:50 +0530

    Create variable script and declare variables

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## If we assign a name to a set of data is called as Variable
+
+# Syntax: VARNAME=DATA
+a=100     # Number
+b=apple   # String
+c=true    # Boolean
+
+# Shell does not have any data type by default
+
