bda85f88905444c64f515f6b70b74567af7502d5
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2021-12-09 06:59:33 +0530

    Add root user check in frontend script

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -54,10 +54,10 @@
 
 
 read -p 'Enter your age: ' age
-#if [ -z "$age" ]; then
-#  echo Input Missing
-#  exit
-#fi
+if [ -z "$age" ]; then
+  echo Input Missing
+  exit
+fi
 
 if [ ! -z "${age}" -a "${age}" -lt 18 ]; then
   echo You are a Minor
