a8387e19a30b65c519e1faf8037b6fd24221e19b
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2021-12-09 06:40:54 +0530

    conditions

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -54,6 +54,11 @@
 
 
 read -p 'Enter your age: ' age
+if [ -z "$age" ]; then
+  echo Input Missing
+  exit
+fi
+
 if [ "${age}" -lt 18 ]; then
   echo You are a Minor
 elif [ "${age}" -gt 60 ]; then
